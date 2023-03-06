7c262573af28b3f41fb4b4d3e261b062a0ed5d9f
Brendan Bosman <bhbosman@gmail.com>
2023-03-06 15:17:09 +0200

    refactoring

--- socksStack/createStackState.go
+++ socksStack/createStackState.go
@@ -5,7 +5,7 @@ import (
 	"github.com/bhbosman/goCommsDefinitions"
 	"github.com/bhbosman/gocommon/GoFunctionCounter"
 	"github.com/bhbosman/gocommon/model"
-	common2 "github.com/bhbosman/gocomms/common"
+	"github.com/bhbosman/gocomms/common"
 	"github.com/bhbosman/goerrors"
 	"github.com/reactivex/rxgo/v2"
 )
@@ -13,37 +13,37 @@ import (
 func createStackState(
 	ctx context.Context,
 	goFunctionCounter GoFunctionCounter.IService,
-) *common2.StackState {
-	return &common2.StackState{
-		Id:          goCommsDefinitions.Socks5,
-		HijackStack: false,
-		Create: func() (common2.IStackCreateData, error) {
+) common.IStackState {
+	return common.NewStackState(
+		goCommsDefinitions.Socks5,
+		false,
+		func() (common.IStackCreateData, error) {
 			return NewStackData(
 				ctx,
 				goFunctionCounter,
 			), nil
 		},
-		Destroy: func(connectionType model.ConnectionType, stackData common2.IStackCreateData) error {
+		func(connectionType model.ConnectionType, stackData common.IStackCreateData) error {
 			if sd, ok := stackData.(*data); ok {
 				return sd.Destroy()
 			}
 			return goerrors.InvalidType
 		},
-		Start: func(
-			conn common2.IInputStreamForStack,
-			stackData common2.IStackCreateData,
+		func(
+			conn common.IInputStreamForStack,
+			stackData common.IStackCreateData,
 			ToReactorFunc rxgo.NextFunc,
-		) (common2.IInputStreamForStack, error) {
+		) (common.IInputStreamForStack, error) {
 			if sd, ok := stackData.(*data); ok {
 				return conn, sd.Start(ctx)
 			}
 			return nil, goerrors.InvalidType
 		},
-		Stop: func(stackData interface{}) error {
+		func(stackData interface{}) error {
 			if sd, ok := stackData.(*data); ok {
 				return sd.Stop()
 			}
 			return goerrors.InvalidType
 		},
-	}
+	)
 }
